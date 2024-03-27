66b446b829e796014dec0b390c608bfccd4635a3
Saravanan B1 <saravanan.10724547@ltimindtree.com>
2024-03-27 09:54:56 +0530

    SpringBootAI

---
+++ src/main/java/com/SpringAI/SpringBootAiWithOpenAiApplication.java
@@ -0,0 +1,13 @@
+package com.SpringAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAiWithOpenAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAiWithOpenAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringAI/SpringBootAiWithOpenAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAiWithOpenAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
